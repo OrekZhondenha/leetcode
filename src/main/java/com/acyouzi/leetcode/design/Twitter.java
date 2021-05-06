1741bfc40e33ab74a96096d2547394231c9c531e
OrekZhondenha <xx553312389@163.com>
2021-05-06 22:46:03 +0800

    auto

---
+++ src/main/java/com/acyouzi/leetcode/design/Twitter.java
@@ -0,0 +1,24 @@
+package com.acyouzi.leetcode.design;
+
+import java.util.List;
+
+/**
+ * Created by SUN on 2017/9/5.
+ */
+public class Twitter {
+//  public Twitter() {
+//
+//  }
+//  public void postTweet(int userId, int tweetId) {
+//
+//  }
+//  public List<Integer> getNewsFeed(int userId) {
+//
+//  }
+//  public void follow(int followerId, int followeeId) {
+//
+//  }
+//  public void unfollow(int followerId, int followeeId) {
+//
+//  }
+}
